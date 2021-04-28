aae361598b7b80f1ea69cdef460e4b78283e01b8
Geran Wen <skran711g@gmail.com>
2021-04-27 20:33:28 -0500

    exercise 3.6, phonebook backend step6 done

--- index.js
+++ index.js
@@ -44,6 +44,59 @@ app.get('/api/persons', (request, response) => {
    response.json(persons)
 })
 
+app.get('/api/info', (request, response) => {
+   response.send(
+      `<p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`
+   )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+   const id = Number(request.params.id)
+   const person = persons.find(p => p.id === id)
+   if (person) {
+      response.json(person)
+   } else {
+      response.status(404).json({
+         error: 'content missing'
+      })
+   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+   const id = Number(request.params.id)
+   persons = persons.filter(p => p.id !== id)
+   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+   const body = request.body
+   if (!body.name) {
+      return response.status(400).json({
+         error: 'name missing'
+      })
+   }
+   if (!body.number) {
+      return response.status(400).json({
+         error: 'number missing'
+      })
+   }
+   if (persons.some(p => p.name === body.name)) {
+      return response.status(409).json({
+         error: 'name must be unique'
+      })
+   }
+
+   const person = {
+      name: body.name,
+      number: body.number,
+      id: Math.floor(Math.random() * 99999) + 101,
+   }
+   persons = persons.concat(person)
+   response.json(person)
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
